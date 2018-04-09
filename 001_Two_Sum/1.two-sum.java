38242c325124334683cd2e3f111720de94051ee7
loveexception <maodajun327@hotmail.com>
2018-04-09 14:58:52 +0800

    code

--- 001_Two_Sum/1.two-sum.java
+++ 001_Two_Sum/1.two-sum.java
@@ -27,6 +27,23 @@
  */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+    	List list = Arrays.asList(nums);
+    	int[] result = new int[2];
+    	result[0]=-1;
+    	result[1]=-1;
+    	for(int i=0;i< nums.length ; i ++){
+    		int want = target- nums[i] ;
+    		for(int j= nums.length-1 ; j==i ;i--){
+    			if(nums[j]==want){
+    				result[0]= i;
+    				result[1]=j;
+    				return result;
+    			}
+
+    		}
+
+    	}
+    	return result;
         
     }
 }
